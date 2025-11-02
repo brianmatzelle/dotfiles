e61e5883b6694196cd26da0f9d67fc011d24d1f1
brianm <bmatzel1@binghamton.edu>
2025-11-02 11:06:22 -0500

    started populating universal aliases. todo-setup symlink to source universal instead of copy-pasting manually

--- zsh/universal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-# TODO: add a subprocess that checks for updates to dotfiles, and runs `git pull` if updates are found
---
+++ zsh/universal_aliases.sh
@@ -0,0 +1,19 @@
+# these aliases work for both bash and zsh.
+# append your ~/.bash_aliases or ~/.zsh_aliases file with them
+
+
+# dev tools
+## git
+alias gcm="git commit -m"
+alias gp="git pull"
+alias main="git switch main"
+## yarn
+alias ys="yarn start"
+alias yrd="yarn run dev"
+alias yt="yarn test"
+## tmux
+alias ta="tmux attach -t"
+alias tn="tmux new -s"
+alias tk="tmux kill-session -t"
+
+alias cat="bat -pp"
